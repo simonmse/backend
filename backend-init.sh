6724c80363a77a2fcc9ebe2763478cf8d01e7c87
simonmse <92433618+simonmse@users.noreply.github.com>
2021-10-17 01:17:55 +0200

    updated adress of the databases

--- backend-init.sh
+++ backend-init.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install default-jdk
 sudo apt install maven
-sudo mvn spring-boot:run -Dspring-boot.run.arguments=--spring.datasource.url=jdbc:mysql://127.0.0.1:3306/db_counter
+sudo mvn spring-boot:run -Dspring-boot.run.arguments=--spring.datasource.url=jdbc:mysql://10.0.0.1:3306/db_counter
