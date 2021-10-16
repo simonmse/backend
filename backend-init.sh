bbaecb963407692ab56319832550ae3320722fa5
RomainAgostinelli <31654911+RomainAgostinelli@users.noreply.github.com>
2021-10-16 18:08:35 +0200

    Update backend-init.sh

--- backend-init.sh
+++ backend-init.sh
@@ -1,6 +1,4 @@
-apt update
-apt install default-jdk
-apt install maven
-echo "Did you change the IP?"
-pause
-mvn spring-boot:run -Dspring-boot.run.arguments=--spring.datasource.url=jdbc:mysql://127.0.0.1:3306/db_counter
+sudo apt update
+sudo apt install default-jdk
+sudo apt install maven
+sudo mvn spring-boot:run -Dspring-boot.run.arguments=--spring.datasource.url=jdbc:mysql://127.0.0.1:3306/db_counter
