497347433d7717328d7b46230732cf0cdb4f1b99
simonmse <92433618+simonmse@users.noreply.github.com>
2021-10-17 14:08:28 +0200

    removed password from user

--- init.sql
+++ init.sql
@@ -3,5 +3,5 @@ USE db_counter;
 
 CREATE TABLE counter(id INTEGER PRIMARY KEY, value INTEGER);
 
-CREATE USER 'mysql'@'%' IDENTIFIED BY 'mysql';
-GRANT ALL PRIVILEGES ON `db_counter` . * TO 'mysql'@'%';
+CREATE USER 'mysql'@'%';
+GRANT ALL PRIVILEGES ON `db_counter` . * TO 'mysql'@'%';s
