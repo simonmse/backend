24696d1ddb0b9ea5316dcccba082578540427a38
simonmse <92433618+simonmse@users.noreply.github.com>
2021-10-17 15:12:37 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -3,5 +3,5 @@ USE db_counter;
 
 CREATE TABLE counter(id INTEGER PRIMARY KEY, value INTEGER);
 
-CREATE USER 'mysql'@'%';
-GRANT ALL PRIVILEGES ON db_counter.* TO 'mysql'@'%';
+CREATE USER 'mysql'@'10.0.0.2' IDENTIFIED BY 'mysql';
+GRANT ALL PRIVILEGES ON db_counter.* TO 'mysql'@'10.0.0.2';
