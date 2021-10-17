1fde6f68abeaa0ede7edb4137dea625627980043
simonmse <92433618+simonmse@users.noreply.github.com>
2021-10-17 14:12:00 +0200

    update syntax

--- init.sql
+++ init.sql
@@ -4,4 +4,4 @@ USE db_counter;
 CREATE TABLE counter(id INTEGER PRIMARY KEY, value INTEGER);
 
 CREATE USER 'mysql'@'%';
-GRANT ALL PRIVILEGES ON `db_counter` . * TO 'mysql'@'%';s
+GRANT ALL PRIVILEGES ON `db_counter`.* TO 'mysql'@'%';
